32370959d805a23191194a6fc9757279302fba6b
LeonardoDionizio <leodionizio13@gmail.com>
2017-09-20 09:38:21 -0300

    adicionado nova pagina contact e algumas configurações em geral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { ContactPage } from './../pages/contact/contact';
 import { ListPage } from '../pages/list/list';
 
 @Component({
@@ -22,7 +23,9 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
+      { title: 'Contact', component: ContactPage },
       { title: 'List', component: ListPage }
+      
     ];
 
   }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ContactPage } from './../contact/contact';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +11,23 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  public pushPage(): void{
+    this.navCtrl.push(ContactPage, {
+      type: 'push',
+      course: 'Ionic',
+      year: 2017,
+      message: () => {
+        console.log('Ionic é vida');
+      }
+    });
+  }
+
+  public setRoot(): void{
+    this.navCtrl.setRoot(ContactPage, {
+      type: 'setRoot'
+    });
   }
 
 }
